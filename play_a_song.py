b0815de5f6e4e52a90be69755e9550c4374f21f8
Kevin Keegan <kevinkeegans@gmail.com>
2019-02-19 19:32:18 +0000

    Big update

--- play_a_song.py
+++ play_a_song.py
@@ -1,9 +1,15 @@
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode
+from threading import Thread
+import queue
 import json
 import time
+
 CLIENT_ID_64 = "ZGE1M2ExOTk3YWE5NDM3Y2JlYTU5ZGFmZWYyY2NjNmE6NjhkODk2YWE2ZDc3NGI2ZDg1OTEzNDdmODE4YzEwMGY="
 REFRESH_TOKEN = 'AQDMlwPy2vH1rfW0NqItf5U97yFtCwXc-Tj9mRRppyp9xMzy190ch1CBKy-twlply58-l-TszZy3eqB4F5yRuVGrDYzTE6xlISWN49Qa6D5dMDPZHaPmTrahrRadRyH6TNWHlQ'
+
+FORBIDDEN_ARTISTS = ['smash mouth', 'ariana grande']
+
 def request_access_token():
     url = 'https://accounts.spotify.com/api/token'
     parameters = {'grant_type' : 'refresh_token', 'refresh_token' : REFRESH_TOKEN}
@@ -13,7 +19,7 @@ def request_access_token():
     response =  json.loads(urlopen(request).read())
     return response['access_token']
 
-def search_tracks(query, access_token):
+def search_track(query, access_token):
     url = 'https://api.spotify.com/v1/search'
     headers = {'Authorization': 'Bearer '+access_token}
     parameters = {'q' : query, 'type': 'track'}
@@ -37,23 +43,92 @@ def play_tracks(track_uris, access_token):
 	
 def im_feeling_lucky(song_query):
     access_token = request_access_token()
-    search_result = search_tracks(song_query, access_token)
+    search_result = search_track(song_query, access_token)
     if search_result:
         track = search_result[0]
         duration_ms = track['duration_ms']
         play_tracks([track['uri']], access_token)
-        print()
         message = "Playing {} by {} from the album {}".format(track['name'], track['artists'][0]['name'], track['album']['name'])
     else:
         message = "No match found for " + song_query
     return message
-	
+
+class MusicQueue:
+    def __init__(self):
+        self.track_queue = queue.Queue()
+        self.track_history = []
+        self.skip_sleep = False
+        self.running_ind = 0
+        
+        self.queue_dispatcher = Thread(target=self.run, args=(int(self.running_ind),))
+        self.queue_dispatcher.start()
+
+    def get_queue(self):
+        print("get_Queue enter")
+        with self.track_queue.mutex:
+            return list(self.track_queue.queue)
+        print("get_Queue exit")
+        
+    def clear(self):
+            print("clear enter")
+            with self.track_queue.mutex:
+                self.skip_sleep = True
+                self.track_queue.queue.clear()
+            print("clear exit")
+            
+    def skip(self):
+            print("skip enter")
+            with self.track_queue.mutex:
+                self.skip_sleep = True
+            print("skip exit")
+            
+    def add(self, song_query):
+        access_token = request_access_token()
+        search_result = search_track(song_query, access_token)
+        if search_result:
+            track = search_result[0]
+            if track['artists'][0]['name'].lower() in FORBIDDEN_ARTISTS:
+                message = "That artist is strictly forbidden"
+            else:
+                self.track_queue.put(track)
+                self.track_history.append(track)
+                message = "Track: {}\nArtist: {}\nAlbum: {}".format(track['name'], track['artists'][0]['name'], track['album']['name'])
+        else:
+            message = "No match found for " + song_query
+        return message     
+
+    def run(self, ind):
+        while True:
+            track_uris = []
+            print("getting from queue")
+            track = self.track_queue.get()
+            print("got from queue")
+            if self.reset:
+                print("reset cleared")
+                with self.track_queue.mutex:
+                    self.skip_sleep = False
+            track_duration_sec = track['duration_ms']/1000.0
+            track_uris.append(track['uri'])
+            print("pushing: " +track['name'])
+            access_token = request_access_token()
+            play_tracks(track_uris, access_token)
+            t1 = time.perf_counter()
+            elapsed = 0
+            print("entering sleep loop")
+            while not self.skip_sleep and elapsed < track_duration_sec:
+                sleep_time = min(0.5, track_duration_sec - elapsed)
+                time.sleep(sleep_time)
+                elapsed = time.perf_counter() - t1
+                    
+                
+
+         
 if __name__ == "__main__":
     access_token = request_access_token()
     track_uris = []
     for i in range(5):
         query= input("Enter a song name: ")
-        track = search_tracks(query, access_token)[0]
+        track = search_track(query, access_token)[0]
         album_name = track['album']['name']
         duration_ms = track['duration_ms']
         track_uris.append(track['uri'])
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, render_template
 import play_a_song
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
@@ -13,15 +13,39 @@ def send_css(path):
 @app.route('/fonts/<path:path>')
 def send_fonts(path):
     return send_from_directory('fonts', path)
+@app.route('/queue')
+def send_queue():
+    up_next = music_queue.get_queue()
+    if up_next:
+        text_str = "UP NEXT:\n"
+        for i, track in enumerate(up_next):
+            text_str += "{}. {}\n".format(i+1,track['name'])
+        output_list = text_str[:-1].split('\n')
+    else:
+        output_list = ["0 Songs in queue."]
+    html =  render_template("text_window.html",text =output_list )
+    return html
+@app.route('/clear_queue')
+def send_clear():
+    music_queue.clear()
+    return "cleared"
+
+@app.route('/skip')
+def send_skip():
+    music_queue.skip()
+    return "skipped"
 
 @app.route('/', methods=['GET'])
 def root():
-	if 'name' in request.args:
-		print(request.args.get('name'))
-		return play_a_song.im_feeling_lucky(request.args.get('name'))
-	else:
-		return app.send_static_file('index.html')
+    if 'name' in request.args:
+        print(request.args.get('name'))
+        response_text = music_queue.add(request.args.get('name'))
+        html =  render_template("text_window.html",text = response_text.split("\n"))
+        return html
+    else:
+        return app.send_static_file('index.html')
 	
-	
+
 if __name__ == "__main__":
+    music_queue = play_a_song.MusicQueue()
     app.run(debug=False,host='0.0.0.0')
